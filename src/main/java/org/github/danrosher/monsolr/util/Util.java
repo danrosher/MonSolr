b935ae4f21cb806c7b9b78859dd943a34b9b8893
Dan Rosher <rosherd@gmail.com>
2020-11-10 14:25:11 +0000

    more helper utilities

--- src/main/java/org/github/danrosher/monsolr/util/Util.java
+++ src/main/java/org/github/danrosher/monsolr/util/Util.java
@@ -3,6 +3,7 @@ package org.github.danrosher.monsolr.util;
 import org.tomlj.TomlParseResult;
 
 import java.util.Optional;
+import java.util.function.Supplier;
 
 public class Util {
 
@@ -10,7 +11,23 @@ public class Util {
         return Optional.ofNullable(config.getLong(key)).orElse(Long.valueOf(def)).intValue();
     }
 
+    public static String getOption(TomlParseResult config, String key, String def){
+        return Optional.ofNullable(config.getString(key)).orElse(def);
+    }
+
     public static long getOption(TomlParseResult config, String key, Long def){
         return Optional.ofNullable(config.getLong(key)).orElse(def);
+    }
+
+    public  static String getRequired(TomlParseResult config, String key) throws Throwable {
+        return getOrThrow(config,key,() -> new IllegalArgumentException("Must set "+key));
+    }
+
+    public  static String getRequired(TomlParseResult config, String key, String msg) throws Throwable {
+        return getOrThrow(config,key,() -> new IllegalArgumentException(msg));
+    }
+
+    public  static String getOrThrow(TomlParseResult config, String key, Supplier<? extends Throwable> sup) throws Throwable {
+        return Optional.ofNullable(config.getString(key)).orElseThrow(sup);
     }
 }
